b112d8351bdfa2cbe4e18bcc3d4f1321c36f7c8d
aliNejati-bu <elecrocellco@gmail.com>
2021-08-23 14:34:15 +0430

    fix bug required

--- database/model/user.js
+++ database/model/user.js
@@ -10,9 +10,9 @@ module.exports = mongoose.model("user",mongoose.Schema({
 
 module.exports.insertValidator = INPUT =>{
     const schema = Joi.object({
-        username:Joi.string().min(2).max(255),
-        email:Joi.string().min(5).max(255),
-        password:Joi.string().min(8).max(255),
+        username:Joi.string().min(2).max(255).required(),
+        email:Joi.string().min(5).max(255).required(),
+        password:Joi.string().min(8).max(255).required(),
     });
 
     return schema.validate(INPUT);
