549cdea9db59be956f8dab5a5962273e0bc38dbb
aliNejati-bu <elecrocellco@gmail.com>
2021-08-23 08:46:50 +0430

    config express

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const db = require("./database/connect");
 const config = require("config");
+const express = require("express");
+
+
+
+const app = express();
 
 db.connect(config.get("db_url"))
     .then(()=>{console.log("connected to database...")})
     .catch(err => console.log(err));
+
+
+app.listen(
+    config.get("port"),
+    config.get("server"),
+    ()=>{
+        console.log("server started on : " + config.get("server")+ ":" + config.get("port"))
+    });
