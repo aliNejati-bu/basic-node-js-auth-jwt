6d7a694d986a9b81d0e1283b6b97990b2bdf34b4
aliNejati-bu <elecrocellco@gmail.com>
2021-08-28 02:00:16 +0430

    add fullUser middleware

---
+++ middlewares/fullUser.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const secretKey = config.get("secret");
+
+module.exports = (req, res, next) => {
+    const token = req.header("x-auth-token");
+
+    if (!token) {
+        return res.status(401).send("401 Unauthorized");
+    }
+
+    const user = jwt.verify(token,secretKey);
+    if (!user) {
+        return res.status(401).send("401 Unauthorized");
+    }
+
+    req.user = user;
+
+    if (!user.roll.includes("admin")) {
+        return res.status(401).send("403 error");
+    }
+
+    next();
+
+
+}
